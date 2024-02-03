dfae654a63c36990087ce86feb26399619206538
“Kracher2” <kracher009@gmail.com>
2024-02-03 15:22:00 +0100

    Twttr

---
+++ test_twttr.py
@@ -0,0 +1,7 @@
+from twttr import shorten
+
+
+def test_all():
+    assert shorten("hello world") == "hll wrld"
+    assert shorten("HELLO WORLD") == "HLL WRLD"
+    assert shorten("H?LL0 W*rlD") == "H?LL0 W*rlD"
